d24f93308ced1cddd9b1ccd09425b712d5cbcd42
ViniMata <vinicius.matareli2005@gmail.com>
2025-05-13 16:36:08 -0300

    chore: removendo testes antigos

--- teste_config.py
+++
@@ -1,7 +0,0 @@
-from app.config import Config
-
-print(Config.SECRET_KEY)
-print(Config.DB_PASSWORD)
-print(Config.DB_HOST)
-print(Config.DB_USER)
-print(Config.DB_NAME)
--- teste_db.py
+++
@@ -1,9 +0,0 @@
-from app.database import get_conexão_db
-
-connection = get_conexão_db()
-
-if connection and connection.is_connected():
-    print("Conseguiu se conectar.")
-    connection.close()
-else:
-    print("Falaha ao tentar se conectar.")
